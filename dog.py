7dbf3b983dab1fbd6a54ec34ff97aa725fd0e6fd
Aleia Knight <aleiaknight@gmail.com>
2019-11-27 12:58:55 -0800

    finished page 1

---
+++ dog.py
@@ -0,0 +1,11 @@
+# dog.py
+class Dog:
+    # Required properties are defined inside the __init__ constructor method
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    # Methods are defined as their own named functions inside the class
+    # Remember to put the "self" parameter every time we make a class method!
+    def bark(self):
+        print("Woof!")
