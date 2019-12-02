df5e5b340d0b356b0f59839f36f9d8e2a92ff26b
Aleia Knight <aleiaknight@gmail.com>
2019-12-02 14:52:09 -0800

    finished page 3, hero fighting

--- superheroes.py
+++ superheroes.py
@@ -58,20 +58,77 @@ class Hero:
 
         self.abilities.append(ability)
 
+    def add_armor(self, armor):
+        '''Add armor to self.armors
+            Armor: Armor Object
+        '''
+        self.armors.append(armor)
+
+    def attack(self):
+        '''Calculate the total damage from all ability attacks.
+          return: total_damage:Int
+        '''
+        # start our total out at 0
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
+
+    def defend(self, damage_amt):
+        '''Calculate the total block amount from all armor blocks.
+            return: total_block:Int
+        '''
+        total_block = 0
+        for damage_amt in self.armors:
+            total_block += damage_amt.block()
+        return total_block
+
+    def take_damage(self, damage):
+        '''Updates self.current_health to reflect the damage minus the defense.
+        '''
+        defense = self.defend(damage)    
+        self.current_health -= damage - defense
+
+    def is_alive(self):  
+        '''Return True or False depending on whether the hero is alive or not.
+        '''
+        if self.current_health <= 0:
+            return False
+        return True
+
+    def fight(self, opponent):
+        # TODO: Fight each hero until a victor emerges.
+        # Phases to implement:
+        # 0) check if at least one hero has abilities. If no hero has abilities, print "Draw"
+        # 1) else, start the fighting loop until a hero has won
+        # 2) the hero (self) and their opponent must attack each other and each must take damage from the other's attack
+        # 3) After each attack, check if either the hero (self) or the opponent is alive
+        # 4) if one of them has died, print "HeroName won!" replacing HeroName with the name of the hero, and end the fight loop
+        if len(self.abilities) == 0 and len(opponent.abilities) == 0:
+            print("Draw")
+        else:
+            while self.is_alive() == True and opponent.is_alive() == True:
+                opponent.take_damage(self.attack())
+                self.take_damage(opponent.attack())
+                if opponent.is_alive() == False: #if opponent loses
+                    print("Winner is: ", self.name)
+                else: #if opponent wins
+                    print("Winner is: ", opponent.name)
 
 
 
 if __name__ == "__main__":
     # If you run this file from the terminal
-    # this block is executed.
-    ability = Ability("Debugging Ability", 50)
-    print(ability.name)
-    print(ability.attack())
-    armor = Armor("Debugging Shield", 10)
-    print(armor.name)
-    print(armor.block())
-    hero = Hero("Grace Hopper", 200)
-    print(hero.name)
-    print(hero.current_health)
-    hero.add_ability(ability)
-    print(hero.abilities)
+    # this block of code is executed.
+
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 130)
+    ability2 = Ability("Super Eyes", 190)
+    ability3 = Ability("Wizard Wand", 240)
+    ability4 = Ability("Wizard Beard", 200)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
